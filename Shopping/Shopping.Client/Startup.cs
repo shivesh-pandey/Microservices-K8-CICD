a7ed7348dd25f68183eb15a4da14a1a8a0f1c446
Shivesh Kumar Pandey <shivesh.viet@gmail.com>
2023-12-27 00:21:50 -0500

    Finished Docker

--- Shopping/Shopping.Client/Startup.cs
+++ Shopping/Shopping.Client/Startup.cs
@@ -24,7 +24,8 @@ namespace Shopping.Client
         {
             services.AddHttpClient("ShoppingAPIClient", client =>
             {
-                client.BaseAddress = new Uri("http://localhost:5000");
+                //client.BaseAddress = new Uri("http://localhost:8000");
+                client.BaseAddress = new Uri(Configuration["ShoppingAPIUrl"]);
             });
             services.AddControllersWithViews();
         }
